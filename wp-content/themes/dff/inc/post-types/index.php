06996d49d003e5f0b704b9a2aa0bfc5e1da8da7a
Yurii Boiko <y.boyko.team@gmail.com>
2022-04-20 12:34:01 +0300

    feature/add-post-type-quiz

--- wp-content/themes/dff/inc/post-types/index.php
+++ wp-content/themes/dff/inc/post-types/index.php
@@ -4,3 +4,4 @@ require_once realpath( __DIR__ . '/insights.php' );
 require_once realpath( __DIR__ . '/programs.php' );
 require_once realpath( __DIR__ . '/future-talks.php' );
 require_once realpath( __DIR__ . '/courses.php' );
+require_once realpath( __DIR__ . '/quizzes.php' );
