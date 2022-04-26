609a656ac0b482874d87d7625b8fc579d45c2681
Ivan Chumak <ivan.chumak@usoftware.co>
2022-04-26 08:56:57 +0000

    fixed ajax error

--- wp-content/themes/dff/includes/courses/my-courses/parts-course/modules-course.php
+++ wp-content/themes/dff/includes/courses/my-courses/parts-course/modules-course.php
@@ -32,9 +32,9 @@
                                     <?php
                                     if (have_rows('course_module_repeater')) :
                                         $row_count = count(get_field('course_module_repeater'));
+                                           
                                         while (have_rows('course_module_repeater')) : the_row();
                                         // echo 'count' . $count_lessons = count(get_sub_field('course_lesson_repeater'));    
-                                        echo 'count' . $count_lessons = 4;    
                                             $module_or_exam = get_sub_field('module_or_exam');
                                             $module_name    = get_sub_field('module_name');
                                             $module_i       = get_row_index();
@@ -56,6 +56,7 @@
                                                
 
                                                 if ($module_or_exam == 'module') {
+                                                    echo 'count' . $count_lessons = count(get_sub_field('course_lesson_repeater')); 
                                                 ?>
                                                     <div class="accordion-head" count-lesson-row="<?php echo $count_lessons ? $count_lessons : ''; ?>">
                                                         <h6><?php _e('Module', 'dff'); ?> <?php echo $module_i . $dff_show_date; ?></h6>
@@ -64,11 +65,13 @@
                                                         <div class="accordion-content">
                                                             <?php
                                                             if (have_rows('course_lesson_repeater')) :
+                                                              
                                                             ?>
                                                                 <ul>
                                                                     <?php
                                                                     while (have_rows('course_lesson_repeater')) : the_row();
                                                                         // $lesson_name = get_sub_field('lesson_name');
+
                                                                         $lesson_or_test = get_sub_field('lesson_or_test');
                                                                         $lesson_test_id = get_sub_field('lesson_test_id');
                                                                         $lesson_i = get_row_index();
