9e03519d0e23b6c0413814595e33793d2b803161
Ivan Chumak <ivan.chumak@usoftware.co>
2022-04-26 07:46:15 +0000

    fixed warning

--- wp-content/themes/dff/single-courses.php
+++ wp-content/themes/dff/single-courses.php
@@ -12,7 +12,7 @@ $posttype = get_post_type();
 			$courses_format_value = $courses_format['value'];
 			$courses_format_label = $courses_format['choices'][$courses_format_value];
 			$course_complexities = get_field_object('course_complexities');
-			if ($img = get_image_by_id($courses->ID)) $src = $img[0];
+			if ($img = get_image_by_id(get_the_ID())) $src = $img[0];
 			else $src = '';
 			$home_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="17.333" height="16" viewBox="0 0 17.333 16">
 			<path fill="currentColor" id="Icon_ionic-md-home" data-name="Icon ionic-md-home" d="M10.042,20.5V15.167h4V20.5h4.067v-8h2.6l-8.667-8-8.667,8h2.6v8Z" transform="translate(-3.375 -4.5)"></path>
