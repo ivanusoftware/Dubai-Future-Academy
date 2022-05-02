f7baf156f54cff6ce7f3867fc558f19e521f405f
Ivan Chumak <ivan.chumak@usoftware.co>
2022-05-02 16:28:51 +0000

    add arabic language

--- wp-content/themes/dff/includes/courses/popular-courses.inc.php
+++ wp-content/themes/dff/includes/courses/popular-courses.inc.php
@@ -48,9 +48,9 @@
                                                 $course_start = get_sub_field('course_start');
                                                 $course_finish = get_sub_field('course_finish');
                                                 // Load field value and convert to numeric timestamp.                                                
-                                                $date_course_start  = date_i18n("j M Y", strtotime($course_start));
-                                                $date_course_finish = date_i18n("j M Y",  strtotime($course_finish));
-                                                echo  '<h5>' .  $date_course_start . ' - ' .  $date_course_finish . '</h5>';
+                                                // $date_course_start  = date_i18n("j M Y", strtotime($course_start));
+                                                // $date_course_finish = date_i18n("j M Y",  strtotime($course_finish));
+                                                echo  '<h5>' .  $course_start . ' - ' .  $course_finish . '</h5>';
                                             endwhile; ?>
                                         <?php endif; ?>
                                     <?php
