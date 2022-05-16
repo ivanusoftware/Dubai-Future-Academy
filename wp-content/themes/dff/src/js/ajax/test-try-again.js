ebbb1684765fc05c8908c1177f747dcb05f75e6d
Ivan Chumak <ivan.chumak@usoftware.co>
2022-05-16 11:31:50 +0000

    add button try againe to test etc

--- wp-content/themes/dff/src/js/ajax/test-try-again.js
+++ wp-content/themes/dff/src/js/ajax/test-try-again.js
@@ -1,8 +1,9 @@
 import dffAccordion from '../scripts/accordion';
 import dffAjaxLessons from '../scripts/ajax-lessons';
-import dffNextBackButtonsActive from '../scripts/next-back-buttons-active';
+// import dffNextBackButtonsActive from '../scripts/next-back-buttons-active';
 import dffTryAgainActiveMainTab from '../scripts/try-again-active-main-tab';
 import dffTryAgainActive from '../scripts/try-again-active';
+import dffTryAgaineButton from '../scripts/try-again-button';
 /**
  * The click functionality the button "try again"
  * to pass the test for the module.
@@ -28,7 +29,39 @@ jQuery(document).on('click', '.exam-footer .test-try-again', function (e) {
         $(".my-courses-tabs-content .tab-wrapper ").html(response);
         dffAccordion();
         dffAjaxLessons(moduleIndex, lessonIndex, lessonTestId, courseId);
-        dffNextBackButtonsActive(moduleIndex, lessonIndex);
+
+        dffTryAgaineButton(moduleIndex, lessonIndex);
+
+        dffTryAgainActiveMainTab(tabId);
+        dffTryAgainActive(moduleIndex);
+    }).fail(function (response) {
+        console.log(response);
+    });
+});
+
+jQuery(document).on('click', '.course-quiz__buttons .module-test-try-again', function (e) {
+    e.preventDefault(); 
+    console.log('module-test-try-again');
+    const moduleIndex = $(this).attr('module-index');
+    const lessonIndex = $(this).attr('lesson-index');
+    const lessonTestId = $(this).attr('lesson-test-id');
+    const courseId = $("#content").find(".my-courses-tabs").attr('course-id');
+    const tabId = $(this).attr('tab-id');
+    console.log(tabId);
+    const data = {
+        action: "tabs_lesson_ajax",
+        main_tab_id: tabId,
+        course_id: courseId,
+    };
+    $.ajax({
+        type: "POST", 
+        url: courses_ajax.url,
+        data: data,
+    }).done(function (response) {
+        $(".my-courses-tabs-content .tab-wrapper ").html(response);
+        dffAccordion();
+        dffAjaxLessons(moduleIndex, lessonIndex, lessonTestId, courseId);   
+        dffTryAgaineButton(moduleIndex, lessonIndex);
         dffTryAgainActiveMainTab(tabId);
         dffTryAgainActive(moduleIndex);
     }).fail(function (response) {
--- wp-content/themes/dff/src/js/scripts/next-back-buttons-active.js
+++ wp-content/themes/dff/src/js/scripts/next-back-buttons-active.js
@@ -7,13 +7,19 @@
 export default function dffNextBackButtonsActive(moduleIndex, lessonIndex) {
     const $ = jQuery.noConflict();
     const tabAccordionItem = $('.accordion-content ul li');
+    console.log();
     tabAccordionItem.each(function () {
         const accordionModuleIndex = $(this).attr('module-index');
         const accordionLessonIndex = $(this).attr('lesson-index');
         if (accordionModuleIndex === moduleIndex && accordionLessonIndex === lessonIndex) {
+            console.log('Tetsts');
             $(this).addClass('active');
         } else {
             $(this).removeClass('active');
         }
     });
 }
+
+
+
+
