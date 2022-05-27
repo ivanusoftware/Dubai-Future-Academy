49639179c85530725032892c2233c39f3dfaa838
Ivan Chumak <ivan.chumak@usoftware.co>
2022-05-27 14:41:19 +0000

    progres leave-course

--- wp-content/themes/dff/includes/functions/ajax-leave-course.php
+++ wp-content/themes/dff/includes/functions/ajax-leave-course.php
@@ -3,7 +3,7 @@
 function leave_course_ajax_callback()
 {
 
-    $course_id       = $_POST['course_id'];
+    $course_id = $_POST['course_id'];
     dff_delete_course_from_user($course_id);
     wp_send_json_success();
     wp_die();
