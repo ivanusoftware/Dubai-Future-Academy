f8ac07717093ccbc73c2f234d06633367576e5b2
Ivan Chumak <ivan.chumak@usoftware.co>
2022-05-30 15:42:35 +0000

    change logic for accordion

--- wp-content/themes/dff/src/js/scripts/accordion.js
+++ wp-content/themes/dff/src/js/scripts/accordion.js
@@ -5,20 +5,27 @@
 const dffAccordion = () => {
     const $ = jQuery.noConflict();
     $('.accordion .open-module .accordion-head').add('.single-course .accordion .accordion-head').on('click', function () {
-        if ($(this).hasClass('active')) {
-            $(this).siblings('.accordion-content').slideUp();
-            $(this).removeClass('active');
-            // $(this).siblings('.accordion-content').slideToggle();
-            // $(this).toggleClass('active');
-            console.log('active1');
-        }
-        else {
-            console.log('active2');
+        const $this = $(this);
+        if (!$this.hasClass('active')) {
             $('.accordion-content').slideUp();
             $('.accordion-head').removeClass('active');
-            $(this).siblings('.accordion-content').slideToggle();
-            $(this).toggleClass('active');
         }
+        $this.toggleClass('active');
+        $this.next().slideToggle();
+        // if ($(this).hasClass('active')) {
+        //     $(this).siblings('.accordion-content').slideUp();
+        //     $(this).removeClass('active');
+        //     $(this).siblings('.accordion-content').slideToggle();
+        //     $(this).toggleClass('active');
+        //     console.log('active1');
+        // }
+        // else {
+        //     console.log('active2');
+        //     $('.accordion-content').slideUp();
+        //     $('.accordion-head').removeClass('active');
+        //     $(this).siblings('.accordion-content').slideToggle();
+        //     $(this).toggleClass('active');
+        // }
     });
 }
 
