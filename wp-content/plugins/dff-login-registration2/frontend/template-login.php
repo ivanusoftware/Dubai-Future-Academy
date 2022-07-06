e8fdedfa4f44fca6fa0bf1f7d5e69483e23f3b0e
Ivan Chumak <ivan.chumak@usoftware.co>
2022-07-06 11:51:27 +0000

    fixing use file get content

--- wp-content/plugins/dff-login-registration2/frontend/template-login.php
+++ wp-content/plugins/dff-login-registration2/frontend/template-login.php
@@ -1,8 +1,9 @@
 <?php
 // echo 'Login';
 // echo plugin_dir_url( __FILE__ );
+echo plugin_dir_path( __FILE__ );
 
 // echo plugin_dir_url( __FILE__ ) . 'build/index.html';
 
-echo file_get_contents(plugin_dir_url( __FILE__ ) . 'build/index.html');
+echo file_get_contents(plugin_dir_path( __FILE__ ) . 'build/index.html');
 // include plugin_dir_url( __FILE__ ) . 'build/index.html';
